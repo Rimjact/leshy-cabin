50742900205556584aa39567c1a5b7769e5372c4
Rimjact <Rimjact@ya.ru>
2026-01-07 22:00:11 +0300

    feat(ClickboxComponent): add custom script for handle mouse clicks on nodes

---
+++ scripts/clickbox_component.gd
@@ -0,0 +1,27 @@
+class_name ClickboxComponent
+extends Area2D
+## Класс компонента для обработки нажатий кнопок мыши
+
+
+## (Сигнал) Нажатие левой кнопки мыши
+signal mouse_left_button_clicked(source: ClickboxComponent)
+## (Сигнал) Нажатие правой кнопки мыши
+signal mouse_right_button_clicked(source: ClickboxComponent)
+
+
+## Обработчик события ввода
+func _input_event(viewport: Viewport, event: InputEvent, _shape_idx: int) -> void:
+	if event is not InputEventMouseButton:
+		return
+	
+	var mouse_button: InputEventMouseButton = event
+	if mouse_button.pressed:
+		return
+	
+	match mouse_button.button_index:
+		MOUSE_BUTTON_LEFT:
+			emit_signal("mouse_left_button_clicked", self)
+			viewport.set_input_as_handled()
+		MOUSE_BUTTON_RIGHT:
+			emit_signal("mouse_right_button_clicked", self)
+			viewport.set_input_as_handled()
