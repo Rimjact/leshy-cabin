454b28ad48f51c357437abea6078152145cb2ff4
Rimjact <Rimjact@ya.ru>
2026-01-09 01:13:29 +0300

    fix(PlayerCardsController): fix card to card hover

--- scripts/player_cards_controller.gd
+++ scripts/player_cards_controller.gd
@@ -6,30 +6,20 @@ extends Node2D
 ## Карточки у игрока
 var cards: Array[CardBase] 
 
-## Наведён ли в данный момент курсор на карточку
-var _is_card_hovered: bool = false
-
-
 ## Когда на карточку навёлся курсор
 func _on_card_cursor_entered(card: CardBase) -> void:
-	if _is_card_hovered:
-		return
 	if card.state != Global.CardState.IN_HAND:
 		return
 	
 	EventBus.card_hover_started.emit(card)
-	_is_card_hovered = true
 
 
 ## Когда курсор ушёл с карточки
 func _on_card_cursor_exited(card: CardBase) -> void:
-	if not _is_card_hovered:
-		return
 	if card.state != Global.CardState.IN_HAND_HOVERED:
 		return
 	
 	EventBus.card_hover_stopped.emit(card)
-	_is_card_hovered = false
 
 
 ## Присоединяет к сигналам Шины
