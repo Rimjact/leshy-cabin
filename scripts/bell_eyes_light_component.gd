9605bd65e87d755297c5757f3336686c072b9166
Rimjact <Rimjact@ya.ru>
2026-01-18 19:34:31 +0300

    test(BellEyesLightComponent): remove debug code

--- scripts/bell_eyes_light_component.gd
+++ scripts/bell_eyes_light_component.gd
@@ -12,7 +12,6 @@ extends PointLight2D
 func _on_bell_state_changed(new_state: Global.BellState) -> void:
 	var new_state_int: int = new_state
 	var new_light_color: Color = _eyes_light_colors[new_state_int]
-	print("color changed to " + var_to_str(new_light_color))
 	_set_light_color(new_light_color)
 
 
