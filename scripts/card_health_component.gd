ee76401f6ae1ef33e5ce229e7d91a6eccf88284a
Rimjact <Rimjact@ya.ru>
2026-02-01 15:19:51 +0300

    feat(CardHealthComponent): create card health component

---
+++ scripts/card_health_component.gd
@@ -0,0 +1,9 @@
+class_name CardHealthComponent
+extends Node2D
+## Компонент здоровья карточки
+
+
+## Очки здоровья
+@export var health: int
+## Счётчик очков здоровья на карточке
+@export var counter_label: Label
