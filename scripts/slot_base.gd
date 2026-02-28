50c66bd1c18b1e5c0a04f1aefec5ad147555aac7
Rimjact <Rimjact@ya.ru>
2026-02-28 22:12:18 +0300

    feat(SlotBase): add on slot attacked logic

--- scripts/slot_base.gd
+++ scripts/slot_base.gd
@@ -24,7 +24,39 @@ func _on_slot_card_placed(slot: SlotBase, new_card: CardBase) -> void:
 	card = new_card
 
 
+## Когда слот атакован
+func _on_slot_attacked(attack_info: AttackSlotInfo) -> void:
+	if attack_info.victime_slot != self:
+		return
+	
+	if not card:
+		_redirect_attack_to_owner(attack_info.damage)
+		return
+	
+	_redirect_attack_to_card(attack_info)
+
+
+## Перенаправляет атаку на владельца слота
+func _redirect_attack_to_owner(damage: int) -> void:
+	match side:
+		Global.BattleSide.PLAYER:
+			EventBus.battle_player_attacked.emit(damage)
+		Global.BattleSide.OPPONENT:
+			EventBus.battle_opponent_attacked.emit(damage)
+
+
+## Перенаправляет атаку на находящуюся в слоте карточку
+func _redirect_attack_to_card(attack_info: AttackSlotInfo) -> void:
+	var damage: int = attack_info.damage
+	var attacker: CardBase = attack_info.attacker
+	var victime: CardBase = card
+	var attack_card_info := AttackCardInfo.new(damage, attacker, victime)
+	
+	EventBus.card_attacked.emit(attack_card_info)
+
+
 ## Соединяет сигналы Шины
 func _connect_signals() -> void:
 	EventBus.slot_cursor_left_button_clicked.connect(_on_slot_cursor_left_button_clicked)
 	EventBus.slot_card_placed.connect(_on_slot_card_placed)
+	EventBus.slot_attacked.connect(_on_slot_attacked)
