5569a2087054e9e7944f6daa641ec16a2445b961
Rimjact <Rimjact@ya.ru>
2026-03-22 18:56:59 +0300

    feat(CardAbilitiesComponent): add ability check methods for targeting and attack

--- scripts/card_abilities_component.gd
+++ scripts/card_abilities_component.gd
@@ -3,6 +3,11 @@ extends Node2D
 ## Класс компонента способностей карточки
 
 
+## Возвращает способность по её уникальному имени
+func get_ability(ability_name: StringName) -> CardAbility:
+	return get_meta(ability_name, null)
+
+
 ## Возвращает способность карточки выбирать цели
 func get_targeting_ability() -> CardAbilityTargeting:
 	var ability: CardAbility = get_ability(&"CardAbilityTargeting")
@@ -21,11 +26,16 @@ func get_attack_ability() -> CardAbilityAttack:
 	return null
 
 
-## Возвращает способность по её уникальному имени
-func get_ability(ability_name: StringName) -> CardAbility:
-	return get_meta(ability_name, null)
-
-
-## Проверяет, имеет ли способность по её уникальному имени
+## Проверяет, имеет ли карточка способность по её уникальному имени
 func has_ability(ability_name: StringName) -> bool:
 	return has_meta(ability_name)
+
+
+## Проверяет, имеет ли карточка способность выбирать цели
+func has_targeting_ability() -> bool:
+	return has_ability(&"CardAbilityTargeting")
+
+
+## Проверяет, имеет ли карточка способность атаковать
+func has_attack_ability() -> bool:
+	return has_ability(&"CardAbilityAttack")
