de4dc74bfa5fd97a73897f28f6ff9bc2c8288548
Rimjact <Rimjact@ya.ru>
2026-03-22 20:30:56 +0300

    feat(AttackSlotInfo): add ignore card var

--- scripts/attack_slot_info.gd
+++ scripts/attack_slot_info.gd
@@ -8,9 +8,12 @@ var damage: int
 var attacker: CardBase
 ## Атакованный слот
 var victime_slot: SlotBase
+## Игнорировать карточку на слоте
+var ignore_card: bool
 
 
-func _init(dmg: int, atck: CardBase, vict_slot: SlotBase) -> void:
+func _init(dmg: int, atck: CardBase, vict_slot: SlotBase, ignore_crd: bool = false) -> void:
 	self.damage = dmg
 	self.attacker = atck
 	self.victime_slot = vict_slot
+	self.ignore_card = ignore_crd
