f3e8bb9270968aa224f4e4a837f839c94fb1ea1c
Rimjact <Rimjact@ya.ru>
2026-03-22 23:16:47 +0300

    feat(CardAbilitiesComponent): add icons positioning funcs

--- scripts/card_abilities_component.gd
+++ scripts/card_abilities_component.gd
@@ -3,6 +3,15 @@ extends Node2D
 ## Класс компонента способностей карточки
 
 
+## Уникальные способности карточки
+var _unique_abilities: Array[CardAbility]
+
+
+func _on_child_order_changed() -> void:
+	_update_unique_abilities_from_children()
+	_update_abilites_icons_position()
+
+
 ## Возвращает способность по её уникальному имени
 func get_ability(ability_name: StringName) -> CardAbility:
 	return get_meta(ability_name, null)
@@ -53,3 +62,37 @@ func has_attack_ability() -> bool:
 ## Проверяет, имеет ли карточка способность защищаться
 func has_defence_ability() -> bool:
 	return has_ability(&"CardAbilityDefence")
+
+
+## Обновляет массив уникальных способностей из дочерних нод
+func _update_unique_abilities_from_children() -> void:
+	_unique_abilities.clear()
+	
+	var abilities_nodes := get_children()
+	for ability_node in abilities_nodes:
+		if not ability_node is CardAbility:
+			continue
+		
+		var ability: CardAbility = ability_node
+		if ability.is_unique:
+			_unique_abilities.append(ability)
+
+
+## Обновляет позиции и размер иконок способностей на карточке
+func _update_abilites_icons_position() -> void:
+	if _unique_abilities.size() == 0:
+		return
+	
+	if _unique_abilities.size() == 1:
+		var ability_node: CardAbility = _unique_abilities[0]
+		ability_node.position = Vector2(0.0, 0.0)
+		ability_node.scale = Vector2(1.0, 1.0)
+		return
+	
+	var first_ability_node: CardAbility = _unique_abilities[0]
+	first_ability_node.position = Vector2(-6.0, -3.0)
+	first_ability_node.scale = Vector2(0.75, 0.75)
+	
+	var second_ability_node: CardAbility = _unique_abilities[1]
+	second_ability_node.position = Vector2(6.0, 3.0)
+	second_ability_node.scale = Vector2(0.75, 0.75)
--- scripts/card_ability.gd
+++ scripts/card_ability.gd
@@ -2,3 +2,7 @@
 class_name CardAbility
 extends Node2D
 ## Абстрактный класс способности карточки
+
+
+## Является ли способность уникальной
+@export var is_unique: bool = false
