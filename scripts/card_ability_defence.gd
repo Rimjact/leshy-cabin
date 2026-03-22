4602fc49e0ebfeb200482b9a9a03e43e9bd1278f
Rimjact <Rimjact@ya.ru>
2026-03-22 21:36:29 +0300

    feat(CardAbilityDefence): create abstract ability

---
+++ scripts/card_ability_defence.gd
@@ -0,0 +1,25 @@
+@abstract
+class_name CardAbilityDefence
+extends CardAbility
+## Абстрактный класс способности карточки защищаться
+
+
+@abstract
+## Выполняет способность защиты карточки
+func defence() -> void
+
+
+## Регистрирует способность карточки в компонент
+func _enter_tree() -> void:
+	assert(owner is CardBase)
+	
+	if owner is CardBase:
+		var card: CardBase = owner
+		card.abilities_component.set_meta(&"CardAbilityDefence", self)
+
+
+## Убирает регистрацию способности карточки из компонента
+func _exit_tree() -> void:
+	if owner is CardBase:
+		var card: CardBase = owner
+		card.remove_meta(&"CardAbilityDefence")
