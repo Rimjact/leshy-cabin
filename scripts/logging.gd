f0439f0b645653c0705951d49f2541a85b4d97ad
Rimjact <Rimjact@ya.ru>
2026-03-27 22:53:19 +0300

    feat(Logging): create static class

---
+++ scripts/logging.gd
@@ -0,0 +1,14 @@
+class_name Logging
+## Класс отвечающий за работу с игровым логгером
+
+
+## Возвращает новый <code>GameLogger</code>
+static func create_logger(name: String) -> GameLogger:
+	var logger := GameLogger.new(name)
+	return logger
+
+
+## Возвращает новый <code>GameLoggerHandlerConsole</code>
+static func create_console_handler() -> GameLoggerHandlerConsole:
+	var handler := GameLoggerHandlerConsole.new()
+	return handler
