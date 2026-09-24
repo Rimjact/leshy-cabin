2b9ee4bee62cbae483bd15c89ca889a614a8b744
Rimjact <Rimjact@ya.ru>
2026-09-24 19:26:45 +0300

    feat(CardAbilitySpawn): write abstract class

---
+++ scripts/card_ability_spawn.gd
@@ -0,0 +1,25 @@
+@abstract
+class_name CardAbilitySpawn
+extends CardAbility
+## Абстрактный класс способности карточки спавнить другие карточки
+
+
+@abstract
+## Выполняет перемещение на слот
+func spawn_cards_at(summoner: CardBase, slots: Array[SlotBase]) -> void
+
+
+## Регистрирует способность карточки в компонент
+func _enter_tree() -> void:
+	assert(owner is CardBase)
+	
+	if owner is CardBase:
+		var card: CardBase = owner
+		card.abilities_component.set_meta(&"CardAbilitySpawn", self)
+
+
+## Убирает регистрацию способности карточки из компонента
+func _exit_tree() -> void:
+	if owner is CardBase:
+		var card: CardBase = owner
+		card.remove_meta(&"CardAbilitySpawn")
