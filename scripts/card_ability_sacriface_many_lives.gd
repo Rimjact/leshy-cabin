924292fd74a743861d6f177b8d40ba4732f2d407
Rimjact <Rimjact@ya.ru>
2026-09-24 20:35:53 +0300

    feat(CardAbilitySacrifaceManyLives): write script and create node

---
+++ scripts/card_ability_sacriface_many_lives.gd
@@ -0,0 +1,7 @@
+class_name CardAbilitySacrifaceManyLives
+extends CardAbilitySacriface
+## Класс способности приношения в жертву краточки с множеством жизней 
+
+
+func sacriface(card: CardBase) -> void:
+	pass
