c375b3e18c3132eab94dc09400972bd5667c57be
Rimjact <Rimjact@ya.ru>
2026-09-24 19:21:27 +0300

    feat(CardAbilityMovement): write abstaract class

---
+++ scripts/card_ability_movement.gd
@@ -0,0 +1,25 @@
+@abstract
+class_name CardAbilityMovement
+extends CardAbility
+## Абстрактный класс способности карточки перемещатся
+
+
+@abstract
+## Выполняет перемещение на слот
+func move_to(card: CardBase, slot: SlotBase) -> void
+
+
+## Регистрирует способность карточки в компонент
+func _enter_tree() -> void:
+	assert(owner is CardBase)
+	
+	if owner is CardBase:
+		var card: CardBase = owner
+		card.abilities_component.set_meta(&"CardAbilityMovement", self)
+
+
+## Убирает регистрацию способности карточки из компонента
+func _exit_tree() -> void:
+	if owner is CardBase:
+		var card: CardBase = owner
+		card.remove_meta(&"CardAbilityMovement")
